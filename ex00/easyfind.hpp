98519ecbc2951c030d8980b8c4cb80a5dbdb0b75
t-ecker <152566019+t-ecker@users.noreply.github.com>
2024-12-06 20:49:57 +0100

    added base files of ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tecker <tecker@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 12:23:18 by tecker            #+#    #+#             */
+/*   Updated: 2024/12/06 17:50:36 by tecker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <string.h>
+#include <iostream>
+
+template<typename T>
+void easyfind(T a, int i)
+{
+	
+}
+
+#endif
